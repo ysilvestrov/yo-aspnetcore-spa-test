7b14cd6526d9b17c8781b33e9e7cf01f4270e124
Yuriy Silvestrov <yuriy@silvestrov.com>
2016-09-04 13:21:33 +0300

    Some changes

--- ClientApp/routes.ts
+++ ClientApp/routes.ts
@@ -4,9 +4,9 @@ import { FetchData } from './components/fetch-data/fetch-data';
 import { Counter } from './components/counter/counter';
 
 export const routes: RouterConfig = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'fetch-data', pathMatch: 'full' },
+    { path: 'fetch-data', component: FetchData },	
     { path: 'home', component: Home },
     { path: 'counter', component: Counter },
-    { path: 'fetch-data', component: FetchData },
-    { path: '**', redirectTo: 'home' }
+    { path: '**', redirectTo: 'fetch-data' }
 ];
